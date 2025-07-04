6c8759118409e01b4c2162af4843865a7c4c0dcd
Trung <trungnnde180575@fpt.edu.vn>
2025-07-04 10:59:32 +0700

    Update notificationRoutes.js

--- BackEnd/routes/notificationRoutes.js
+++ BackEnd/routes/notificationRoutes.js
@@ -1,9 +1,9 @@
-const notificationController = require('../controllers/notificationController');
+const notificationController = require("../controllers/notificationController");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/:userId', notificationController.getNotifications);
+router.get("/:userId", notificationController.getNotifications);
 
-router.put('/:userId', notificationController.updateAllStatusNotification);
+router.put("/:userId", notificationController.updateAllStatusNotification);
 
 module.exports = router;
